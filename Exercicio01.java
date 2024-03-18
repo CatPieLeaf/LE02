05ea11b4ae36d9674875fa3a9dc7ef8baa8a3bc8
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-18 10:14:44 -0300

    Exercicio 1

--- Exercicio01.java
+++ Exercicio01.java
@@ -5,6 +5,20 @@ public class Exercicio01 {
     // na tela quais valores que são menores, iguais ou superiores à média
 
     public static void run() {
-        
+        int[] nums = new int[5];
+        int media = 0;
+        int[] diff = new int[5];
+        for (int i = 0; i < 5; i++) {
+            nums[i] = Prompt.lerInteiro("Insira um número:");
+            media += nums[i];
+        }
+        media = media / 5;
+        Prompt.imprimir("\nMédia Aritmética: " + media + "\n");
+        for (int i = 0; i < 5; i++) {
+            diff[i] = nums[i] - media;
+            if (diff[i] < 0) Prompt.imprimir("Valor: " + nums[i] + ", Inferior à média, Diferença: " + diff[i]);
+            if (diff[i] == 0) Prompt.imprimir("Valor: " + nums[i] + ", Igual à média, Diferença: " + diff[i]);
+            if (diff[i] > 0) Prompt.imprimir("Valor: " + nums[i] + ", Superior à média, Diferença: " + diff[i]);
+        }
     }
 }
