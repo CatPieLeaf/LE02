8f8b4873ebf2663a88e1952fbf6cf352de893c81
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-18 11:21:28 -0300

    Exercicio 5

--- Exercicio05.java
+++ Exercicio05.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Exercicio05 {
 
     // Elabore um programa que leia 5 números do teclado e os armazene em um vetor.
@@ -5,6 +7,16 @@ public class Exercicio05 {
     // que o número n aparece no vetor
 
     public static void run() {
-        
+        int[] nums = new int[5];
+        int ocorrencias = 0;
+        for (int i = 0; i < 5; i++) {
+            nums[i] = Prompt.lerInteiro("Digite um número para sua sequência: ");
+        }
+        Prompt.imprimir("Sua sequencia: " + Arrays.toString(nums).replace("[", "").replace("]", "") +  "\n");
+        int n = Prompt.lerInteiro("Insira um número n: ");
+        for (int i = 0; i < 5; i++) {
+            if (nums[i] == n) ocorrencias++;
+        }
+        Prompt.imprimir("Ocorrências de n: " + ocorrencias);
     }
 }
