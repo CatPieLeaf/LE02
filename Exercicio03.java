a3381ed6e326314aaa2fd030e22b27ff3dfa405e
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-18 10:29:02 -0300

    Exercicio 3

--- Exercicio03.java
+++ Exercicio03.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Exercicio03 {
 
     // Faça um programa que leia em um vetor uma sequência finita de números digitados
@@ -5,6 +11,19 @@ public class Exercicio03 {
     // primeiro vetor e depois apresente os valores deste vetor.
 
     public static void run() {
-        
+        String seq = new String();
+        seq = Prompt.lerLinha("Insira uma sequencia de números separados por espaços:");
+        Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(seq);
+        // -?\d+(\.\d+)?
+        List<String> ocorrencias = new ArrayList<>();
+        while (matcher.find()) {
+            ocorrencias.add(matcher.group());
+        }
+        double[] dobro = new double[ocorrencias.size()]; 
+        for (int i = 0; i < ocorrencias.size(); i++) {
+            dobro[i] = (Double.parseDouble(ocorrencias.get(i))) * 2;
+        }
+        Prompt.imprimir(Arrays.toString(dobro).replace("[", "").replace("]", ""));
     }
 }
