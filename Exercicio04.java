5d4b7479b359205c58f7f5236c288a98253d07c9
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-18 11:03:10 -0300

    Exercicio 4

--- Exercicio04.java
+++ Exercicio04.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Exercicio04 {
 
     // Elabore um programa que leia um vetor de 5 números digitados pelo usuário e uma
@@ -5,6 +7,14 @@ public class Exercicio04 {
     // são inferiores a n.
 
     public static void run() {
-        
+        int[] nums = new int[5];
+        for (int i = 0; i < 5; i++) {
+            nums[i] = Prompt.lerInteiro("Digite um número para sua sequência: ");
+        }
+        Prompt.imprimir("Sua sequencia: " + Arrays.toString(nums).replace("[", "").replace("]", "") +  "\n");
+        int n = Prompt.lerInteiro("Insira um número n: ");
+        for (int i = 0; i < n; i++) {
+            Prompt.imprimir("Indice: "+ i + "," + " Número: " + nums[i]);
+        }
     }
 }
