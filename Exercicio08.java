92692bd46dd5474b62c9f27863acf6db6dd06c6d
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-20 10:47:34 -0300

    Exercicio 8

--- Exercicio08.java
+++ Exercicio08.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Exercicio08 {
 
     // Dados dois vetores A e B de 5 elementos cada, criar um vetor C que representa a
@@ -5,6 +7,20 @@ public class Exercicio08 {
     // de B.
 
     public static void run() {
-        
+        int[] a = new int[5];
+        int[] b = new int[5];
+        int[] c = new int[10];
+        Prompt.imprimir("------ CONJUNTO A ------");
+        for (int i = 0; i < 5; i++) {
+            a[i] = Prompt.lerInteiro("Insira um número para o conjunto A: ");
+        }
+        Prompt.imprimir("\n------ CONJUNTO B ------");
+        for (int i = 0; i < 5; i++) {
+            b[i] = Prompt.lerInteiro("Insira um número para o conjunto B: ");
+        }
+        Prompt.imprimir("\n------ CONJUNTO C ------");
+        System.arraycopy(a, 0, c, 0, 5);
+        System.arraycopy(b, 0, c, 5, 5);
+        Prompt.imprimir(Arrays.toString(c).replace("[", "").replace("]", ""));
     }
 }
