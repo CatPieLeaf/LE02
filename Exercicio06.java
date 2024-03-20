a6a7d1b5443fbf2725d8ea1411dd6630a534a6a6
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-20 09:31:04 -0300

    Exercicio 6

--- Exercicio06.java
+++ Exercicio06.java
@@ -10,6 +10,17 @@ public class Exercicio06 {
     // Ex.: notas: 7.5, 9.2 pesos: 6, 4. Média ponderada: (7.5 * 6 + 9.2 * 4) / (6 + 4)
 
     public static void run() {
-        
+        double[] notas = new double[5];
+        double[] pesos = new double[5];
+        double numerador = 0;
+        double denominador = 0;
+        for (int i = 0; i < 5; i++) {
+            notas[i] = Prompt.lerDecimal("Insira o valor da nota " + (i+1));
+            pesos[i] = Prompt.lerDecimal("Insira o valor do peso " + (i+1));
+            numerador += (notas[i] * pesos[i]);
+            denominador += pesos[i];
+        }
+        double media = numerador / denominador;
+        Prompt.imprimir("Média ponderada: " + media);
     }
 }
