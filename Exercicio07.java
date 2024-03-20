3ece5855b8dd866f4024a9d1863aacbfb44b904d
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-20 10:37:17 -0300

    Exercicio 7

--- Exercicio07.java
+++ Exercicio07.java
@@ -5,6 +5,24 @@ public class Exercicio07 {
     // coincidir.
 
     public static void run() {
-        
+        int[] elementos1 = new int[5];
+        int[] elementos2 = new int[5];
+        boolean isEqual = true;
+        Prompt.imprimir("------ CONJUNTO 1 ------");
+        for (int i = 0; i < 5; i++) {
+            elementos1[i] = Prompt.lerInteiro("Insira um número para o conjunto 1: ");
+        }
+        Prompt.imprimir("\n------ CONJUNTO 2 ------");
+        for (int i = 0; i < 5; i++) {
+            elementos2[i] = Prompt.lerInteiro("Insira um número para o conjunto 2: ");
+        }
+        for (int i = 0; i < 5; i++) {
+            if (elementos1[i] != elementos2[i]) isEqual = false;
+        }
+        if (isEqual == true) {
+            Prompt.imprimir("\nOs conjuntos sao exatamente iguais");
+        } else {
+            Prompt.imprimir("\nOs conjuntos sao diferentes");
+        }
     }
 }
