ef1e036b6404e0999359d636f8e8bf225e42ea6c
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-21 11:13:14 -0300

    Exercicio 9

--- Exercicio09.java
+++ Exercicio09.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Exercicio09 {
 
     // Elabore um programa que leia os vetores A e B de 5 elementos e gere um vetor C
@@ -7,6 +9,26 @@ public class Exercicio09 {
     // b. Os elementos das posições ímpares de C são os elementos das posições ímpares de B;
 
     public static void run() {
-        
+        int[] a = new int[5];
+        int[] b = new int[5];
+        int[] c = new int[5];
+        Prompt.imprimir("------ CONJUNTO A ------");
+        for (int i = 0; i < 5; i++) {
+            a[i] = Prompt.lerInteiro("Insira um número para o conjunto A: ");
+        }
+        Prompt.imprimir("\n------ CONJUNTO B ------");
+        for (int i = 0; i < 5; i++) {
+            b[i] = Prompt.lerInteiro("Insira um número para o conjunto B: ");
+        }
+        for (int i = 0; i < 5; i++) {
+            int num = (i+1) % 2;
+            if (num != 0) {
+                c[i] = b[i];
+            } else {
+                c[i] = a[i];
+            }
+        }
+        Prompt.imprimir("\n------ CONJUNTO C ------");
+        Prompt.imprimir(Arrays.toString(c).replace("[", "").replace("]", ""));
     }
 }
