fa5ef4d89197f9f99d612a2487d9451bd87ee66b
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-22 10:11:39 -0300

    Exercicio 13

--- Exercicio13.java
+++ Exercicio13.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Exercicio13 {
 
     // Elabore um algoritmo que receba um número n e retorne um vetor com os n
@@ -5,6 +7,15 @@ public class Exercicio13 {
     // Exemplo: n = 8, vetor = {1, 1, 2, 3, 5, 8, 13, 21}.
 
     public static void run() {
-        
+        int n = 0;
+        n = Prompt.lerInteiro("Digite um número n");
+        long fib[] = new long[n];
+        fib[0] = 1;
+        if (n > 2) fib[1] = 1;
+        for (int i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        Prompt.imprimir("------ FIBONACCI ------");
+        Prompt.imprimir(Arrays.toString(fib).replace("[", "").replace("]", ""));
     }
 }
