c878da49d295ffa308fc36ec4e1b044e0772c4b0
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-22 09:47:39 -0300

    Exercicio 12

--- Exercicio12.java
+++ Exercicio12.java
@@ -5,6 +5,15 @@ public class Exercicio12 {
     // Exemplo: {0, 5, 8, 1, -6, 4, -7, 9, 10, -14, 3, 12} = 8 * 4 * 10 * 12 = 3840
 
     public static void run() {
-        
+        int[] c = new int[12];
+        int produto = 1;
+        Prompt.imprimir("------ CONJUNTO ------");
+        for (int i = 0; i < 12; i++) {
+            c[i] = Prompt.lerInteiro("Insira um número para o conjunto: ");
+        }
+        for (int i = 0; i < 12; i++) {
+            if (c[i] > 0 && c[i] % 2 == 0) produto *= c[i];
+        }
+        Prompt.imprimir("Produto: " + produto);
     }
 }
