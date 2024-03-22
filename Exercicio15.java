46c7d761573e58eadeb6e197bf4a24146e5a0c58
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-22 10:52:13 -0300

    Exercicio 15

--- Exercicio15.java
+++ Exercicio15.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Exercicio15 {
 
     // Elabore um algoritmo que leia 5 números do teclado e preencha um vetor de
@@ -6,6 +8,22 @@ public class Exercicio15 {
     // valor lido só poderá ser maior que 5.
 
     public static void run() {
-        
+        int[] c = new int[5];
+        Prompt.imprimir("------ CONJUNTO ------");
+        for (int i = 0; i < 5; i++) {
+            int num = Prompt.lerInteiro("Insira um número para o conjunto: ");
+            if (i > 0) {
+                if (num > c[i-1]) {
+                    c[i] = num;
+                } else {
+                    Prompt.imprimir("O número deve ser maior que o anterior!");
+                    i--;
+                }
+            } else {
+                c[i] = num;
+            }
+        }
+        Prompt.imprimir("\n------ CONJUNTO ------");
+        Prompt.imprimir(Arrays.toString(c).replace("[", "").replace("]", ""));
     }
 }
