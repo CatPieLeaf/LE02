40ec4160071ea9f00fb0e31eb1de3434041716ff
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-22 10:34:53 -0300

    Exercicio 14

--- Exercicio14.java
+++ Exercicio14.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Exercicio14 {
 
     // Faça um programa que leia um vetor de 5 elementos e, após a leitura, posicione
@@ -5,6 +7,22 @@ public class Exercicio14 {
     // apagado ou duplicado. Apresente o vetor atualizado na tela.
 
     public static void run() {
-        
+        int[] a = new int[5];
+        int highest = 0;
+        int indexhigh = 0;
+        Prompt.imprimir("------ CONJUNTO ------");
+        for (int i = 0; i < 5; i++) {
+            a[i] = Prompt.lerInteiro("Insira um número para o conjunto: ");
+            if (i > 0) if (a[i] > a[i-1]) {
+                highest = a[i];
+                indexhigh = i;
+            }
+        }
+        for (int i = indexhigh; i < 4; i++) {
+            a[i] = a[i+1];
+        }
+        a[4] = highest;
+        Prompt.imprimir("------ CONJUNTO ALTERADO ------");
+        Prompt.imprimir(Arrays.toString(a).replace("[", "").replace("]", ""));
     }
 }
