b58305f0cc0b21cd6a7bb6dc97100bf8c19de37e
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-22 09:30:11 -0300

    Exercicio 11

--- Exercicio11.java
+++ Exercicio11.java
@@ -6,6 +6,20 @@ public class Exercicio11 {
     // Exemplo: {0, 2, 4, 6, 8}, {1, 3, 5, 7, 9} = 0*1 + 2*3 + 4*5 + 6*7 + 8*9 = 140
 
     public static void run() {
-        
+        int[] a = new int[5];
+        int[] b = new int[5];
+        int produto = 0;
+        Prompt.imprimir("------ CONJUNTO A ------");
+        for (int i = 0; i < 5; i++) {
+            a[i] = Prompt.lerInteiro("Insira um número para o conjunto A: ");
+        }
+        Prompt.imprimir("\n------ CONJUNTO B ------");
+        for (int i = 0; i < 5; i++) {
+            b[i] = Prompt.lerInteiro("Insira um número para o conjunto B: ");
+        }
+        for (int i = 0; i < 5; i++) {
+            produto += a[i] * b[i];
+        }
+        Prompt.imprimir("Produto Escalar: " + produto);
     }
 }
