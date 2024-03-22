bddd7ee5a8949c3b5c1a0e0ea58be2d310c84fc9
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2024-03-22 09:21:50 -0300

    Exercicio 10

--- Exercicio10.java
+++ Exercicio10.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Exercicio10 {
 
     // Crie um programa que leia um vetor A de 5 posições e, ao final da leitura, copie
@@ -6,6 +8,18 @@ public class Exercicio10 {
     // por diante.
 
     public static void run() {
-        
+        int[] a = new int[5];
+        int[] b = new int[5];
+        int j = 0;
+        Prompt.imprimir("------ CONJUNTO A ------");
+        for (int i = 0; i < 5; i++) {
+            a[i] = Prompt.lerInteiro("Insira um número para o conjunto A: ");
+        }
+        Prompt.imprimir("\n------ CONJUNTO B ------");
+        for (int i = 4; i >= 0; i--) {
+            b[j] = a[i];
+            j++;
+        }
+        Prompt.imprimir(Arrays.toString(b).replace("[", "").replace("]", ""));
     }
 }
